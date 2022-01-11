18f82dc43b66ed52ecd5e5edf2903e0cbf02cf88
daniellima <daniellima@vxcase.com.br>
2022-01-11 03:14:35 -0300

    Criado crud de fornecedor

--- app/Services/AuthService.php
+++ app/Services/AuthService.php
@@ -2,6 +2,8 @@
 
 namespace App\Services;
 
+use App\Models\User;
+
 class AuthService {
 
     /**
@@ -11,8 +13,8 @@ class AuthService {
      * @return bool|array
      */
     public function getLoginToken(array $data){
-        $token = auth()->attempt($data);
-
+        $token = auth('api')->attempt($data);
+        
         if(!$token) {
             return false;
         }
@@ -20,7 +22,7 @@ class AuthService {
         return [
             'access_token' => $token,
             'token_type' => 'bearer',
-            'expires_in' => auth()->factory()->getTTL() * 60,
+            'expires_in' => auth('api')->factory()->getTTL() * 60,
             'user' => auth()->user()
         ];
     }
---
+++ app/Services/FornecedorService.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Services;
+
+use App\Abstracts\AbstractModelService;
+
+class FornecedorService extends AbstractModelService {
+    
+}
